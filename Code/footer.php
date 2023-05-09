af0e020244aa52a021b9142df244534ce930327a
ThisIsNotVi <114073746+ThisIsNotVi@users.noreply.github.com>
2023-05-09 11:24:08 +0200

    Add files via upload

---
+++ Code/footer.php
@@ -0,0 +1,50 @@
+<!-- de inhoud van dit bestand wordt onderaan elke pagina geplaatst -->
+
+<footer>
+    <hr style="border-color: #24298FFF">
+        <div class="Footer">
+            <footer>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-sm-4 col-md-3 item">
+                            <h3>Navigatie</h3>
+                            <ul>
+                                <li><a href="/nerdygadgets/browse.php?category_id=1">Novelty Items</a></li>
+                                <li><a href="/nerdygadgets/browse.php?category_id=2">Clothing</a></li>
+                                <li><a href="/nerdygadgets/browse.php?category_id=4">T-Shirts</a></li>
+                                <li><a href="/nerdygadgets/browse.php?category_id=6">Computing Novelties</a></li>
+                                <li><a href="/nerdygadgets/browse.php?category_id=7">USB Novelties</a></li>
+                                <li><a href="/nerdygadgets/browse.php?category_id=9">Toys</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-4 col-md-3 item">
+                            <h3>About</h3>
+                            <ul>
+                                <li><a href="#">Company</a></li>
+                                <li><a href="#">Team</a></li>
+                                <li><a href="#">Legacy</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-4 col-md-3 item">
+                            <h3>Nog iets</h3>
+                            <ul>
+                                <li><a href="#">Wat</a></li>
+                                <li><a href="#">Hier</a></li>
+                                <li><a href="#">Doen?</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3 item social"><a href="#"><i class="icon ion-social-twitter" style="padding-right: 10px"></i></a><a href="#"><i class="icon ion-social-facebook" style="padding-right: 10px"></i></a><a href="#"><i class="icon ion-social-instagram" style="padding-right: 10px"></i></a>
+                            <p class="copyright">NerdyGadgets © 2022</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div
+</footer>
+
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
